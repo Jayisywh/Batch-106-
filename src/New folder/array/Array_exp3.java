967dcba17fce786520b82f46eabff0bb61a9167a
User <User@192.168.56.1>
2024-03-13 13:34:38 +0630

    second commit

---
+++ src/New folder/array/Array_exp3.java
@@ -0,0 +1,17 @@
+package array;
+
+public class Array_exp3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] Name = {"y2k clone","ko gyi kyaw","madalay thr","69 mg ly","sar pee pi lr"};
+		//System.out.print(Name.Length;i++)
+		for (int i=0; i<Name.length; i++)
+		{
+			System.out.print(Name[i]);
+		}
+		
+
+	}
+
+}
